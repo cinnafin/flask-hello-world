8893f401efa3fc2100313ee675bd4c6be945f569
Steven Delaney <sjdelane@gmail.com>
2024-03-27 00:02:48 +0000

    edited connection

--- app.py
+++ app.py
@@ -8,6 +8,6 @@ def hello_world():
 
 @app.route('/db_test')
 def testing():
-    conn = psycopg2.connect(postgres://hello_world_sql_user:ACR2qSqGQ68g5SkMzrPQhMIr2kfA9ccy@dpg-co1luc6v3ddc7385e9t0-a/hello_world_sql)
+    conn = psycopg2.connect("postgres://hello_world_sql_user:ACR2qSqGQ68g5SkMzrPQhMIr2kfA9ccy@dpg-co1luc6v3ddc7385e9t0-a/hello_world_sql")
     conn.close()
     return "Database Connection Successful"
