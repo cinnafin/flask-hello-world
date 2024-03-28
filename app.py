01f370feec7a9187d9527490c7bbd74894c4d563
Steven Delaney <sjdelane@gmail.com>
2024-03-28 00:24:08 +0000

    db_create

--- app.py
+++ app.py
@@ -11,3 +11,9 @@ def testing():
     conn = psycopg2.connect('postgres://database_lom7_user:TTJFJYzStJQEE0FaHwisqNn7FUDEWUzC@dpg-co1mhpq0si5c73cov4n0-a.oregon-postgres.render.com/database_lom7')
     conn.close()
     return 'Database Connection Successful'
+
+@app.route('/db_create')
+def create():
+    cconn = psycopg2.connect('postgres://database_lom7_user:TTJFJYzStJQEE0FaHwisqNn7FUDEWUzC@dpg-co1mhpq0si5c73cov4n0-a.oregon-postgres.render.com/database_lom7')
+    conn.close()
+    return 'db_create connection successful'
